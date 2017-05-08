370b45e5e4b4edcb2c0cdb2d8f6364682e63c0ad
klogdog <klogdog@users.noreply.github.com>
2017-05-08 03:03:03 -0400

    Added JSON Object Constructor
    Added a JSON object constructor to dynamically fill fields with measurement data

--- python/pahopythonclient.py
+++ python/pahopythonclient.py
@@ -1,22 +1,49 @@
+#for submitting data to influx db format is measurement, tags{tags}, fields{fields} 
+#for topic format coming out of the microcontroller should be sensors/sensortype/location/measurementtype1,measurementtype2,measurementtypeN 
+#for data format should be datafrom_measurementtype1/datafrom_measurementtype2/datafrom_measurementtypeN
+
+
 import paho.mqtt.client as mqtt
 from influxdb import InfluxDBClient
 
 
 
-mqttServer = "mqttServer" #default is localhost
-mqttPort = 1883 #use 1883 as default
-mqttKeepAlive = 60 #use 60 as default
+mqttServer = "mqttServer"
+mqttPort = "mqttPort" #use 1883 as default
+mqttKeepAlive = "mqttKeepAliveTime" #use 60 as default
 mqttUsername = "mqttUsername"
 mqttPassword = "mqttPassword"
 mqttTopic = "mqttTopic"
 mqttClientID = "mqttClientID"
 
 
-influxDBserver = "influxDBserver" #default is localhost
+influxDBserver = "influxDBserver"
 influxDBport = "influxDBport" #use 8086 as default
 influxDBusername = "influxDBusername" #use root as default
 influxDBpassword = "influxDBpassword" #use root as default
 influxDatabase = "influxDatabase"
+
+
+
+def json_constructor(topic, measurement):
+    fields = "{"
+    topic = topic
+    measurement = measurement
+    sensortype = topic.split("/")[1]
+    location = topic.split("/")[2]
+    splitTopic = topic.split("/")[3]
+    splitMeasurementType = splitTopic.split(",")
+    splitMeasurement = measurement.split("/")
+    for i in range (0,len(splitMeasurementType)):
+       measurementTypeHolder = splitMeasurementType[i]
+       measurementHolder = splitMeasurement[i]
+       fields = fields + measurementTypeHolder + ":" + measurementHolder + ","
+
+    fields = fields[:-1]
+    fields = fields + "}"
+    json_con = "[" + "{" + "measurement" + ":" + sensortype + "," + "tags" + ":" + "{" + "location" + ":" + location + "}" + "," +  "fields" + ":" + fields + "}" + "]"
+    return json_con
+
 
 
 # The callback for when the client receives a CONNACK response from the server.
@@ -29,29 +56,12 @@ def on_connect(client, userdata, flags, rc):
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    
-    measurements = msg.payload
+    measurement = msg.payload
     topic = msg.topic
-    temperature = float(measurements.split("/")[0])
-    humidity = float(measurements.split("/")[1])
-    location = topic.split("/")[1]
-    sensortype = topic.split("/")[2]
-    json_body = [
-    {
-        "measurement": sensortype,
-        "tags": {
-            "location": location,
-        },
-     
-        "fields": {
-            "temperature": temperature,
-            "humidity" : humidity
-        }
-    }
-    ]
+    json_body = json_constructor(topic,measurement)
     influx_client.write_points(json_body)
-    print(msg.topic+" "+str(msg.payload))
-    print json_body
+
+
 
 influx_client = InfluxDBClient(influxDBserver, influxDBport, influxDBusername , influxDBpassword , influxDatabase)
 client = mqtt.Client()

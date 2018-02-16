f53ce1763f4d7b8828fd9830c68c912d1b96814c
klogdog <klogdog@users.noreply.github.com>
2018-02-16 07:10:08 -0700

    Update and rename JetsonTX2publishexample.py to JetsonTX2subscribeexample.py

---
+++ python/JetsonTX2subscribeexample.py
@@ -0,0 +1,38 @@
+#example for Jetson TX2
+
+import paho.mqtt.client as mqtt
+
+
+#edit these to match your settings
+mqttServer = "192.168.1.5" #use localhost as default
+mqttPort = "1883" #use 1883 as default
+mqttKeepAlive = 60 #use 60 as default
+mqttUsername = "nvidia"
+mqttPassword = "nvidia"
+mqttTopic = "data"
+mqttClientID = "jetson1"
+
+
+
+def on_connect(client, userdata, flags, rc):
+    client.subscribe(mqttTopic)
+
+#use this function to do stuff when you receive a message
+#when a message is received it will execute the commands in this function
+def on_message(client, userdata, msg):
+    measurement = msg.payload
+    topic = msg.topic
+    print topic
+    print measurement 
+
+
+client = mqtt.Client()
+client.username_pw_set(mqttUsername,mqttPassword)
+client.on_connect = on_connect
+client.on_message = on_message
+
+
+client.connect(mqttServer, mqttPort, mqttKeepAlive)
+
+
+client.loop_forever()
